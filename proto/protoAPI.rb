6a89ece05acc69f1d6cd47ac6bca9b0177392e44
Uros Jovanovic <uros.jovanovic@cannonical.com>
2015-11-18 10:59:21 +0100

    * initial aggregation

--- proto/protoAPI.rb
+++ proto/protoAPI.rb
@@ -5,8 +5,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'mongo'
 require 'open-uri'
+require 'date'
 
 include Mongo
+
+@@CHARMSTORE_URL = "http://api.jujugui.org/charmstore/v4"
 
 set :bind, '0.0.0.0'
 
@@ -73,6 +76,7 @@ get "/u/:user" do
 	res = userColl.find("_id" => params[:user]).to_a
 	if res.empty? 
 		status 404
+		body '{"error": "user not found"}'
 		return
 	end
 	status 200
@@ -112,6 +116,8 @@ post "/u/:user/store/subs" do
 		body '{"error": "user not found"}'
 		return
 	end
+
+	# XXX check if the id is promulgated charm and get it's original url
 
 	res = subsColl.find("user" => params[:user], "charm_id" => charm_id).to_a
 	if not res.empty?
@@ -155,14 +161,72 @@ end
 # * user notifications
 # ***************************************************
 
-
+get "/u/:user/store/notifications" do
+	udoc = userColl.find("_id" => params[:user]).to_a
+	if udoc.empty? 
+		status 404
+		body '{"error": "user not found"}'
+		return
+	end
+end
 
 # ***************************************************
 # * store changes 
 # ***************************************************
 
-get "/store/changes" do 
+def filterPublished(data, last_access)
+	last = last_access.to_i
+	filtered = []
+	data.each {
+		|ht|
+		id = ht["Id"]
+		stime = ht["PublishTime"]
+		ptime = Time.parse(stime)
+		filtered << {"charm_id" => id, "publish_time" => ptime} if ptime.to_i >= last
+	}
+	return filtered
+end
+
+get "/store/changes/aggregate" do 
 	content_type :json
+
+	last_time = nil
+	last_doc = nil
+	doc = cscColl.find("_id" => "last_cs_access").to_a
+	if doc.empty?
+		now = Time.now.utc		
+		# You've got 7 days!
+		history = 1
+		last_time = Time.new(now.year, now.month, now.day - history, now.hour, now.min, now.sec)
+	else
+		pp doc
+		last_doc = doc[0]
+		last_time = last_doc["time"]
+	end
+
+	pp last_time
+	# Make a request to check all updates from last time.
+	# An example of the call:
+	# https://api.jujucharms.com/charmstore/v4/changes/published?start=2015-11-18
+	now = Time.now.utc
+	query = @@CHARMSTORE_URL + "/changes/published?start=#{last_time.year}-#{last_time.month}-#{last_time.day}"
+	pp query
+	contents = URI.parse(query).read
+	data = JSON.parse(contents)
+	pp data
+
+	data = filterPublished(data, last_time)
+	data.each { |entry| id = cscColl.insert(entry) }
+	pp data.size 
+
+	last_access_data = {"_id" => "last_cs_access", "time" => now}
+	if last_doc.nil?
+		cscColl.insert(last_access_data)
+	else
+		cscColl.update({"_id" => "last_cs_access"}, last_access_data)
+	end
+
 	status 200
 	body ''
 end
+
